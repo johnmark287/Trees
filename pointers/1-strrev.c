3ffeae4b071d86c253465b9b31f37937c355bdec
johnmark287 <johnmark.muhando@students.jkuat.ac.ke>
2023-10-21 23:34:47 +0300

    update

--- pointers/1-strrev.c
+++ pointers/1-strrev.c
@@ -1,32 +1,47 @@
 #include "main.h"
-char *_strrev(char *str, int size)
-{
-    // hello
-    char *ptr1, *ptr2;
-    char *s;
-    int i;
+// char *_strrev(char *str, int size)
+// {
+//     // hello
+//     char *ptr1, *ptr2;
+//     char *s;
+//     int i;
 
-    ptr1 = str;
-    ptr2 =  str + size;
+//     ptr1 = str;
+//     ptr2 =  str + size;
 
-    for (i = 0; i < (size / 2); i++)
-    {
-        *ptr1 = *ptr2;
-        ptr1++;
-        ptr2--;
-    }
-
-    return (str);
-}
+//     for (i = 0; *(str + i); i++)
+//     {
+//         *(ptr2 + i) = *(s + i);
+//         // ptr1++;
+//         ptr2--;
+//     }
+//     return (str);
+// }
 
 int main(void)
 {
-    char *str;
-    int n;
+    int n, i;
+    char *str, s[n], *ptr2;
 
     str = "hello";
-    n = 5;
-    printf("%s", str);
-    _strrev(str, n);
+    n = 4;
+    printf("%s\n", str);
+    // s = _strrev(str, n);
+	// ptr1 = str;
+    ptr2 =  str + n;
+    printf("%c\n", *ptr2);
+	*s = *ptr2;
+	ptr2--;
+	s++;
+	*s = *ptr2;
+        // s[i] = ptr2[n];
+    // for (i = 0; i < 5; i++)
+    // {
+    //     ptr1++;
+    //     ptr2--;
+	// 	n--;
+    // }
+    printf("%c", *s);
+    
     return (0);
 }
